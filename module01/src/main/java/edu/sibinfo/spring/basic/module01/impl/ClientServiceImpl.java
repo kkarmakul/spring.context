e50a9d7f2022e35ec3068c7f895111e4e6e98261
Kirill Karmakulov <karmakulov@improveitgroup.com>
2017-04-21 18:13:41 +0700

    Running application with XML config

--- module01/src/main/java/edu/sibinfo/spring/basic/module01/impl/ClientServiceImpl.java
+++ module01/src/main/java/edu/sibinfo/spring/basic/module01/impl/ClientServiceImpl.java
@@ -9,9 +9,15 @@ public class ClientServiceImpl implements ClientService {
 
 	private ClientDao clientDao;
 	private SmsService smsService;
+
+	public ClientServiceImpl(ClientDao clientDao, SmsService smsService) {
+		super();
+		this.clientDao = clientDao;
+		this.smsService = smsService;
+	}
+
 	public void register(String firstName, String familyName, String phone) {
 		clientDao.save(new Client(familyName, firstName, phone));
 		smsService.send(phone, String.format("%s %s, you were registered", familyName, firstName));
 	}
-
 }
