07b5539a2453543b3065294aa92ead7c95b02c53
Kirill Karmakulov <karmakulov@improveitgroup.com>
2017-04-21 18:47:44 +0700

    Module 02 - start

---
+++ module02/src/main/java/edu/sibinfo/spring/basic/module02/Client.java
@@ -0,0 +1,36 @@
+package edu.sibinfo.spring.basic.module02;
+
+public class Client {
+	private final String familyName;
+	private final String firstName;
+	private final String mobile;
+	
+	public Client(String familyName, String firstName, String mobile) {
+		super();
+		this.familyName = familyName;
+		this.firstName = firstName;
+		this.mobile = mobile;
+	}
+
+	public String getFamilyName() {
+		return familyName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Client [familyName=").append(familyName).append(", firstName=").append(firstName)
+				.append(", mobile=").append(mobile).append("]");
+		return builder.toString();
+	}
+	
+	
+}
---
+++ module02/src/main/java/edu/sibinfo/spring/basic/module02/ClientService.java
@@ -0,0 +1,5 @@
+package edu.sibinfo.spring.basic.module02;
+
+public interface ClientService {
+	void register(String firstName, String familyName, String phone);
+}
