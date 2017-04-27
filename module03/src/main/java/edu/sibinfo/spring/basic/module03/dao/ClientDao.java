cf8724f23c028ee68820848ed4c443aaf7d12394
Kirill Karmakulov <kkarmakul@gmail.com>
2017-04-27 07:24:43 +0700

    Module 03 - start

---
+++ module03/src/main/java/edu/sibinfo/spring/basic/module03/dao/ClientDao.java
@@ -0,0 +1,7 @@
+package edu.sibinfo.spring.basic.module03.dao;
+
+import edu.sibinfo.spring.basic.module03.domain.Client;
+
+public interface ClientDao {
+	void save(Client client);
+}
---
+++ module03/src/main/java/edu/sibinfo/spring/basic/module03/dao/impl/ClientDaoImpl.java
@@ -0,0 +1,27 @@
+package edu.sibinfo.spring.basic.module03.dao.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Repository;
+
+import edu.sibinfo.spring.basic.module03.dao.ClientDao;
+import edu.sibinfo.spring.basic.module03.domain.Client;
+
+@Repository
+public class ClientDaoImpl implements ClientDao {
+	private AtomicLong lastId = new AtomicLong(0L); 
+	private Map<Long, Client> clients = new HashMap<Long, Client>();
+	
+	public void save(Client client) {
+		if (client.getId() == null) {
+			client.setId(lastId.getAndIncrement());
+			clients.put(client.getId(), client);
+			System.out.printf("Added %s. Total: %d", client, clients.size()).println();
+		} else {
+			System.out.printf("Saved %s. Total: %d", client, clients.size()).println();
+		}
+	}
+
+}
