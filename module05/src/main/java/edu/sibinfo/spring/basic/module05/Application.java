e1b23511025b6dd44873a93b752cfbc06b760e17
Kirill Karmakulov <kkarmakul@gmail.com>
2017-05-01 11:37:58 +0700

    Module 05 - implement InitializingBean, DisposableBean

--- module05/src/main/java/edu/sibinfo/spring/basic/module05/Application.java
+++ module05/src/main/java/edu/sibinfo/spring/basic/module05/Application.java
@@ -7,6 +7,7 @@ public class Application {
 	public static void main(String[] args) {
 		try (ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext("edu.sibinfo.spring.basic")) {
 			CustomInitDestroy bean1 = ctx.getBean(CustomInitDestroy.class);
+			CustomInitDestroyIfc bean2 = ctx.getBean(CustomInitDestroyIfc.class);
 			System.out.printf("Application.main: %s%n", bean1);
 		}
 	}
---
+++ module05/src/main/java/edu/sibinfo/spring/basic/module05/CustomInitDestroyIfc.java
@@ -0,0 +1,25 @@
+package edu.sibinfo.spring.basic.module05;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomInitDestroyIfc implements InitializingBean, DisposableBean {
+
+	@Autowired
+	private ApplicationContext appContext;
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.printf("CustomInitDestroyIfc.destroy() %n");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.printf("CustomInitDestroyIfc.afterPropertiesSet: %s%n", appContext);
+	}
+
+}
