a152ef03d281edc5ea9b08a3a73240034ad82c4d
Kirill Karmakulov <kkarmakul@gmail.com>
2017-05-04 08:31:10 +0700

    Module 07 - start

---
+++ module07/src/main/java/edu/sibinfo/spring/basic/module07/service/impl/ClientServiceImpl.java
@@ -0,0 +1,39 @@
+package edu.sibinfo.spring.basic.module07.service.impl;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.sibinfo.spring.basic.module07.dao.ClientDao;
+import edu.sibinfo.spring.basic.module07.domain.Client;
+import edu.sibinfo.spring.basic.module07.service.ClientService;
+import edu.sibinfo.spring.basic.module07.service.SmsService;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+	@Autowired
+	private ClientDao clientDao;
+	@Autowired
+	private SmsService smsService;
+	@Autowired
+	private MessageDigest encoder;
+
+	public Client register(String firstName, String familyName, String phone) {
+		Client client = new Client(familyName, firstName, phone);
+		clientDao.save(client);
+		smsService.send(phone, String.format("%s %s, you were registered", familyName, firstName));
+		return client;
+	}
+
+	@Override
+	public void setPassword(Client client, String password) {
+		client.setPassword(encoder.digest(password.getBytes(StandardCharsets.UTF_8)));
+		clientDao.save(client);		
+	}
+	
+	
+}
